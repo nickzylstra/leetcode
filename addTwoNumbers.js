70d2ded95622e6017a94477f8b9b188be4ebe175
nickzylstra <nick.zylstra@gmail.com>
2020-01-25 17:56:53 -0800

    Remove object creation

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -33,9 +33,17 @@ const addTwoNumbers = function addTwoNumbers(l1, l2) {
   const l3 = new ListNode();
   let node3 = l3;
   while (node1 || node2 || carry) {
-    if (!node1) node1 = { val: 0, next: null };
-    if (!node2) node2 = { val: 0, next: null };
-    let digit = node1.val + node2.val + carry;
+    let val1 = 0;
+    let val2 = 0;
+    if (node1) {
+      val1 = node1.val;
+      node1 = node1.next;
+    }
+    if (node2) {
+      val2 = node2.val;
+      node2 = node2.next;
+    }
+    let digit = val1 + val2 + carry;
     if (digit > 9) {
       digit %= 10;
       carry = 1;
@@ -44,8 +52,6 @@ const addTwoNumbers = function addTwoNumbers(l1, l2) {
     }
     node3.next = new ListNode(digit);
 
-    node1 = node1.next;
-    node2 = node2.next;
     node3 = node3.next;
   }
 
