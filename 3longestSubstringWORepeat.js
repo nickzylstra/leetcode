d15ef6a7cfa6207d2a18f56c7c41d44b391b8f9f
nickzylstra <nick.zylstra@gmail.com>
2020-01-26 14:27:57 -0800

    Pass all tests

--- 3longestSubstringWORepeat.js
+++ 3longestSubstringWORepeat.js
@@ -3,10 +3,33 @@
  * @return {number}
  */
 
-const lengthOfLongestSubstring = function(s) {
-  
+
+const lengthOfLongestSubstring = function lengthOfLongestSubstring(s) {
+  let longLen = 0;
+  const curChars = new Map();
+  const queue = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    const char = s.charAt(i);
+    queue.push(char);
+
+    if (curChars.has(char)) {
+      let queueHead;
+      do {
+        queueHead = queue.shift();
+        curChars.delete(queueHead);
+      } while (queueHead !== char);
+    } else if (queue.length > longLen) {
+      longLen = queue.length;
+    }
+    curChars.set(char, i);
+  }
+
+  return longLen;
 };
 
 console.log(lengthOfLongestSubstring('abcabcbb'), '=== 3');
 console.log(lengthOfLongestSubstring('bbbbbb'), '=== 1');
 console.log(lengthOfLongestSubstring('pwwkew'), '=== 3');
+console.log(lengthOfLongestSubstring('aab'), '=== 2');
+console.log(lengthOfLongestSubstring('dvdf'), '=== 3');
