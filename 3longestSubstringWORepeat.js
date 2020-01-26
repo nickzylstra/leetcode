59dec9fcbd475be37903bfed88af2ce7eca9fb79
nickzylstra <nick.zylstra@gmail.com>
2020-01-26 10:37:01 -0800

    Setup tests

---
+++ 3longestSubstringWORepeat.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const lengthOfLongestSubstring = function(s) {
+  
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'), '=== 3');
+console.log(lengthOfLongestSubstring('bbbbbb'), '=== 1');
+console.log(lengthOfLongestSubstring('pwwkew'), '=== 3');
