b37307c94478e5f080ab193ad67c93302ba2e04f
nickzylstra <nick.zylstra@gmail.com>
2020-01-26 17:04:28 -0800

    Reduce time and space complexity

--- 3longestSubstringWORepeat.js
+++ 3longestSubstringWORepeat.js
@@ -7,22 +7,15 @@
 const lengthOfLongestSubstring = function lengthOfLongestSubstring(s) {
   let longLen = 0;
   const curChars = new Map();
-  const queue = [];
 
-  for (let i = 0; i < s.length; i += 1) {
+  for (let i = 0, j = 0; i < s.length; i += 1) {
     const char = s.charAt(i);
-    queue.push(char);
 
     if (curChars.has(char)) {
-      let queueHead;
-      do {
-        queueHead = queue.shift();
-        curChars.delete(queueHead);
-      } while (queueHead !== char);
-    } else if (queue.length > longLen) {
-      longLen = queue.length;
+      j = Math.max(curChars.get(char), j);
     }
-    curChars.set(char, i);
+    longLen = Math.max(longLen, i - j + 1);
+    curChars.set(char, i + 1);
   }
 
   return longLen;
@@ -33,3 +26,5 @@ console.log(lengthOfLongestSubstring('bbbbbb'), '=== 1');
 console.log(lengthOfLongestSubstring('pwwkew'), '=== 3');
 console.log(lengthOfLongestSubstring('aab'), '=== 2');
 console.log(lengthOfLongestSubstring('dvdf'), '=== 3');
+console.log(lengthOfLongestSubstring('advdaf'), '=== 4');
+console.log(lengthOfLongestSubstring(' '), '=== 1');
