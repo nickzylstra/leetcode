c01d5d02a045cd16e7e70639a16ec1c5a1c8b2f9
nickzylstra <nick.zylstra@gmail.com>
2020-01-27 12:00:32 -0800

    Add new problem

---
+++ 5longestPalindromicSubstring.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+const longestPalindrome = function longestPalindrome(s) {
+
+};
+
+console.log(longestPalindrome('babad'), '=== bab');
+console.log(longestPalindrome('cbbd'), '=== bb');
