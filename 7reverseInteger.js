872845064efee5d24c7c6623e49c3275f9c78780
nickzylstra <nick.zylstra@gmail.com>
2020-01-27 21:48:29 -0800

    Add problem with tests

---
+++ 7reverseInteger.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function (x) {
+
+};
+
+console.log(reverse(123), '=== 321');
+console.log(reverse(-123), '=== -321');
+console.log(reverse(120), '=== 21');
