07e952e4d4f6aabd4f93bec1d3f4a076e1975442
nickzylstra <nick.zylstra@gmail.com>
2020-01-27 20:41:06 -0800

    Add tests

--- 5longestPalindromicSubstring.js
+++ 5longestPalindromicSubstring.js
@@ -4,8 +4,17 @@
  */
 
 const longestPalindrome = function longestPalindrome(s) {
+  let start = 0;
+  let end = 0;
 
+  for (let i = 1; i < s.length; i += 1) {
+    
+  }
+
+  return s.substring(start, end);
 };
 
+console.log(longestPalindrome(''), '=== ');
+console.log(longestPalindrome('b'), '=== b');
 console.log(longestPalindrome('babad'), '=== bab');
-console.log(longestPalindrome('cbbd'), '=== bb');
+console.log(longestPalindrome('cbb'), 'bb');
