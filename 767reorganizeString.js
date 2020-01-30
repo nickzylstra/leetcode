e61d0e3b22c8dae43d0ccc406bc13e0a8aa32ae1
nickzylstra <nick.zylstra@gmail.com>
2020-01-30 09:48:02 -0800

    Add tests for new problem

---
+++ 767reorganizeString.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+const reorganizeString = function (S) {
+
+};
+
+console.log(`"${reorganizeString('aab')}"`, '=== "aba"');
+console.log(`"${reorganizeString('aaab')}"`, '=== ""');
+console.log(`"${reorganizeString('aabaccca')}"`, '=== "cabacacac"');
